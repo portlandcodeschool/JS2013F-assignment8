3e0b4c9c4fde6ebf4d817c6ac5a9aa1c319b629a
abbystevens <adeats@gmail.com>
2013-11-17 15:23:11 -0800

    first try

--- solutions/loops.js
+++ solutions/loops.js
@@ -1,28 +1,78 @@
 module.exports.factorial = function factorial (num) {
 
-  // implement me!
+if (num < 0 || num !== Math.floor(num))
+	return undefined;
 
-  return null;
-}
+var result = 1;
+  for(; num > 1; num--) {
+  	result *= num; 
+  } 
+  return result;
+ };
+
+  
 
 module.exports.capitalizeEveryOther = function capitalizeEveryOther (str) {
+	var result = "";
+	if (str[i] = [a-z]) {
+	  for(var i = 0; i < str.length - 1; i += 2) {
+			//going two characters at a time, so need an if statement for if it has an odd number of characters
+		result += str[i].toLowerCase();
+		result += str[i + 1].toUpperCase();
+	  }
 
-  // implement me!
-
-  return null;
-}
+	  if (i === str.length - 1) {
+		result += str[i].toLowerCase();
+	  }
+  	  return result;
+	} else {
+		result += str[i];
+	}
+};	
 
 module.exports.capitalizeVowels = function capitalizeVowels (str) {
 
-  // implement me!
+  var newArray = "";
 
-  return null;
-}
+  for(var i = 0; i <= str.length - 1; i++) {
+
+    if (str[i] === "a") {
+      newArray += str[i].toUpperCase();
+	} else if (str[i] === "e") {
+	  newArray += str[i].toUpperCase();
+	} else if (str[i] === "i") {
+	  newArray += str[i].toUpperCase();
+	} else if (str[i] === "o") {
+	  newArray += str[i].toUpperCase();
+	} else if (str[i] === "u") {
+	  newArray += str[i].toUpperCase();	
+	} else {
+	  newArray += str[i];
+	}	
+  }  
+return newArray;
+};
+
 
 module.exports.isPalindrome = function isPalindrome (str) {
 
-  // implement me!
+  var reverseStr = "";
 
-  return null;
+  
+  	for (var i = str.length - 1; i >= 0; i--) {
+  	reverseStr += str[i];
+   	};
+    
+    trueFalse();
+    return reverseStr;
 
-}
+
+    function trueFalse (str, reverseStr) {
+      if (str === reverseStr) {
+		console.log(true);     
+	  } else {
+		console.log(false)      
+	  }
+    };  
+
+};

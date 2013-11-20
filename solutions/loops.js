36f526e97fd498c0ddf9f9a07f4f22f3877cfabc
JFingert <joshuafingert@gmail.com>
2013-11-19 19:12:31 -0800

    finished

--- solutions/loops.js
+++ solutions/loops.js
@@ -12,25 +12,48 @@ module.exports.factorial = function factorial (num) {
 }
 
 module.exports.capitalizeEveryOther = function capitalizeEveryOther (str) {
-
   str = str.toLowerCase();
-  //str.split("");
-  if (str.length < 2){
-      str = str;
+  counter = 0;
+  newStr = "";
+  for (var i = 0; i < str.length; i++) {
+    if (/[a-z]/.test(str[i])){
+      counter++;
+    }
+  
+  if (counter % 2 == 0){
+    newStr += str[i].toUpperCase();
   } else {
-  for (var i = 1; i <= str.length;){
-    if (str[i] !== (/[a-z]/)){
-     do {i + 1;
-      } while (str[i] !== (/[a-z]/));
-    } else {
-     str[i] == str[i].replace(str[i], str[i].toUpperCase());
-   } 
-    i +=2;
-  } 
-  return str;
+    newStr += str[i];
+  }
 }
-}  
+return newStr;
+}
 
+//   str = str.toLowerCase();
+//   var newStr = [];
+//   var blahStr = str.split("");
+//   for (var i = 0; i < str.length; i++) {
+//     if (/[a-zA-Z]/.test(blahStr[i]) && i % 2 !== 0) {
+//       blahStr[i] = blahStr[i].toUpperCase();
+//       newStr.push(blahStr[i]);
+//     } else if (/[a-zA-Z]/.test(blahStr[i]) || i % 2 == 0) {
+//       newStr.push(blahStr[i]);
+//       i += 1;
+//     }  else if (/\W/.test(blahStr[i])) {
+
+//       newStr.push(blahStr[i]);
+//       i += 3;
+//     }
+//   }
+//   return newStr.join("");
+// }
+
+
+
+
+ /*var charIgnore (i) {
+  do {i + 1; while (str[i] !== (/\w/));} 
+}*/
 // if (i == (/\W/)){
 //   str = str;
 // } else if (str[i] % 2 !== 0){
@@ -46,56 +69,29 @@ module.exports.capitalizeEveryOther = function capitalizeEveryOther (str) {
 
 module.exports.capitalizeVowels = function capitalizeVowels (str) {
 
-// str.split("");
-// for (var i = 0; i <= str.length; i++){
-//   if (str[i] === (/\W/ || /\s/)){
-//     str[i];
-//   }else if (str[i] === "a" || "A"){
-//     str[i] === "A";
-//   } else if (str[i] === "e" || "E"){
-//     str[i] === "E";
-//   } else if (str[i] === "i" || "I"){
-//     str[i] === "I";
-//   } else if (str[i] === "o" || "O"){
-//     str[i] === "O";
-//   } else if (str[i] === "u" || "U"){
-//     str[i] === "U";
-//   }
-// //var joined = str.join();
-// }
-
-// var str = "Apples are round, and apples are juicy.";
-// var newstr = str.replace("apples", "oranges", "gi");
-// str = str.replace('a', 'A');
-// str = str.replace('e', 'E');
-// str = str.replace('i', 'I');
-// str = str.replace('o', 'O');
-// str = str.replace('u', 'U');
-//str = str;
-str.split("")
-for (var i = 0; i <= str.length; i++){
-  if (str[i] == 'a' || 'e' || 'i' || 'o' || 'u'){
-    str[i] == str[i].replace(/str[i]/i, str[i].toUpperCase());
+  str.split("")
+  var newStr = [];
+  for (var i = 0; i < str.length; i++){
+    if (/[a]|[e]|[i]|[o]|[u]/.test(str[i])){
+      newStr.push(str[i].toUpperCase());
+    } else {
+      newStr.push(str[i]);
+    }
   }
-}
-return str.toString();
+  return newStr.join("");
 
 }
 
 
 module.exports.isPalindrome = function isPalindrome (str) {
-var frontwards, backwards, bool = false;
-for (var i = 0; i<=str.length; i++) {
-  frontwards.push(str[i]);
-}
-for (var i = str.length; i>=0; i--) {
-  backwards.push(str[i]);
-}
-if (frontwards == backwards){
-  bool = true;
-}
-
-
-  return bool;
-
+  var reverse = [];
+  var splitStr = str.split("");
+  for (var i = splitStr.length - 1; i >= 0; i--) {
+    reverse.push(splitStr[i]);
+  }
+  if (str == reverse.join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
